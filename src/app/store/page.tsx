33995e9a200fe14c48b1a1866220ae54b738fa9b
Blue Robin <julianjohnt@gmail.com>
2023-12-21 20:33:44 -0600

    maybe this will make vercel work. I added semicolons

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -6,20 +6,20 @@ export const metadata: Metadata = {
 };
 
 export default function store() {
-    var p = products.products
+    var p = products.products;
+    var products_load = p.map((item, index) => (item.map((item1, index1) =>
+    <div>
+        <h2>{item1.name}</h2>
+    </div>
+    )));
+
     var html = (
         <> 
             <h1>Store</h1>
             
-            {p.map((item, index) => (item.map((item1, index1) =>
-            <div>
-                <h2>{item1.name}</h2>
-            </div>
-            )
-            ))}
-            
+            {products_load}
         </>
-    )
+    );
 
 
     return html;  // Here's what the empty tag fragment means if anyone needs it: https://stackoverflow.com/questions/59751733/react-native-what-exactly-is-the-empty-component
