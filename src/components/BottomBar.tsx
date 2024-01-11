82bcb6c2dc17284a933fdca3139467bb03251fa4
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-11 03:57:54 +0000

    made a bottom navbar for credits ad

---
+++ src/components/BottomBar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+
+export default function Bottonmar() {
+    return (
+        <nav className="bg-zinc-950 p-4 fixed bottom-0 left-0 right-0 z-5  w-full">
+            <ul className="flex justify-between text-emerald-400 mx-10">
+                <div className="flex items-center space-x-6 ">
+                    <li>
+                    Developed by the Allen High School Web Development 
+                    looking to contribute learn more at our websites github page <a href ="https://github.com/Mr-Ben-Merch-Store/Website">HERE</a>!  
+                </li>
+                </div>
+                
+            </ul>
+        </nav>
+    );
+}
