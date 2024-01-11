78daca1a23d11a55bbe6d7046bdbfff57b15eabb
Blue-Robin-Taken <86581171+Blue-Robin-Taken@users.noreply.github.com>
2024-01-11 21:52:44 +0000

    changed tag to footer because that's easier to read

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -5,16 +5,7 @@ import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 export default function Bottombar() {
     return (
-        <nav className="bg-zinc-950 p-4 static bottom-0 z-5 w-full">
-            <ul className="flex justify-between text-emerald-400 mx-10">
-                <div className="flex items-center space-x-6 ">
-                    <li>
-                    Developed by the Allen High School Web Development 
-                    looking to contribute learn more at our websites github page <a className='text-teal-300' href ="https://github.com/Mr-Ben-Merch-Store/Website">HERE</a>!  
-                    </li>
-                </div>
-                
-            </ul>
-        </nav>
-    );
+        <footer className="static bottom-0 bg-zinc-950 p-4 z-5 w-full">Developed by the Allen High School Web Development 
+looking to contribute learn more at our websites github page <a className='text-teal-300' href ="https://github.com/Mr-Ben-Merch-Store/Website">HERE</a>!</footer>
+);
 }
