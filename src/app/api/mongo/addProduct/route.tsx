d3102fe2c0543dca59925f48ce93e1be94d3a737
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-08-24 18:03:58 -0500

    Admin dashboard upload works now!

--- src/app/api/mongo/addProduct/route.tsx
+++ src/app/api/mongo/addProduct/route.tsx
@@ -1,8 +1,7 @@
-import { SignJWT } from 'jose';
-import uploadShirt from '@/lib/mongodb';
+import { uploadShirt } from '@/lib/mongodb/admin';
 
 export async function POST(req: Request) {
     const form = await req.json();
-
-    uploadShirt(form['name'], form['description'], form['imageURL']); // Using a post request sent from client side, we can call a function that uploads the shirt to MongoDB
+    console.log(form['name']);
+    await uploadShirt(form['name'], form['description'], form['imageURL']); // Using a post request sent from client side, we can call a function that uploads the shirt to MongoDB
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import { getShirts } from '@/lib/mongodb/admin';
+import { ShirtsForm } from '@/app/components/adminPage/addProductForms';
 
 export const metadata: Metadata = {
     title: 'Dashboard',
@@ -10,8 +11,10 @@ async function shirts() {
 
     let html = shirts.map((item: any, index: any) => (
         <div key={index}>
-            <h1 className="text-sm">{String(item._id)}</h1>
-            <p>{item.description}</p>
+            <div className="bg-gradient-to-r from-gray-600 to-black min-w-36 rounded-lg shadow-sm">
+                <h2 className="text-lg m-3 p-3">{String(item._id)}</h2>
+                <p>{item.description}</p>
+            </div>
         </div>
     ));
     return html;
@@ -22,8 +25,11 @@ export default async function Dashboard() {
         <>
             <h1>Dashboard</h1>
             <h2>Current products: </h2>
-            {await shirts()}
+            <div className="flex flex-col m-5 p-5 items-center align-center">
+                {await shirts()}
+            </div>
             <h2>Add a Shirt</h2>
+            <ShirtsForm />
         </>
     );
 }
