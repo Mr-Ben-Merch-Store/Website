b551e0a12fb2280ceca1cea9b4dc80a7b8add162
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-08-24 17:18:20 -0500

    Fixed preview (loads env variables slightly differently)

--- src/lib/mongodb/index.ts
+++ src/lib/mongodb/index.ts
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_URI;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@storeproducts.r4z7wse.mongodb.net/?retryWrites=true&w=majority&appName=StoreProducts`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
