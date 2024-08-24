43e30436ab86db5253b2b2a993c06dc79c035c22
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-08-24 12:28:08 -0500

    I forgot to save the file from before...

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -9,11 +9,7 @@ export default function store() {
     return (
         <>
             <h1>Store</h1>
-<<<<<<< HEAD
-=======
             <hr></hr>
-            <ShopCard></ShopCard>
->>>>>>> 972e76b7b92fa836676380fc8f1597e1c2083c6c
         </>
     );
 }
