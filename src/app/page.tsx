c53cea8b11a1937ed0247647a512fd71f4bf6b97
Lizzy Ell <lunar@lunarware.tech>
2024-09-04 19:37:01 +0000

    Use `Image` objects to decrease compile times

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
     title: 'Home',
@@ -14,16 +15,40 @@ export default function Home() {
             <div className="w-1/2 mx-auto overflow-x-auto scrollable-gallery">
                 <div className="whitespace-nowrap scroll-smooth snap-x snap-mandatory flex py-10">
                     <div className="inline-block w-64 h-64 m-4 bg-transparent snap-center flex-shrink-0">
-                        <img src="gallery/bettereagle_chip.png" alt="Concept 1" className="w-full h-full object-cover rounded-lg" />
+                        <Image 
+                            src="/gallery/bettereagle_chip.png" 
+                            alt="Concept 1" 
+                            className="w-full h-full object-cover rounded-lg" 
+                            height={500}
+                            width={500}   
+                        />
                     </div>
                     <div className="inline-block w-64 h-64 m-4 bg-transparent snap-center flex-shrink-0">
-                        <img src="gallery/spacecsdesign.jpg" alt="Concept 2" className="w-full h-full object-cover rounded-lg" />
+                        <Image 
+                            src="/gallery/spacecsdesign.jpg" 
+                            alt="Concept 2" 
+                            className="w-full h-full object-cover rounded-lg" 
+                            height={500}
+                            width={500}
+                            />
                     </div>
                     <div className="inline-block w-64 h-64 m-4 bg-transparent snap-center flex-shrink-0">
-                        <img src="gallery/mrbenword.png" alt="Concept 3" className="w-full h-full object-cover rounded-lg" />
+                        <Image 
+                            src="/gallery/mrbenword.png" 
+                            alt="Concept 3" 
+                            className="w-full h-full object-cover rounded-lg" 
+                            height={500}
+                            width={500}
+                        />
                     </div>
                     <div className="inline-block w-64 h-64 m-4 bg-transparent snap-center flex-shrink-0">
-                        <img src="product images/shirt1.png" alt="Concept 4" className="w-full h-full object-cover rounded-lg" />
+                        <Image 
+                            src="/ProdImages/shirt1.png" 
+                            alt="Concept 4" 
+                            className="w-full h-full object-cover rounded-lg" 
+                            width={500}
+                            height={500}
+                            />
                     </div>
                 </div>
             </div>
