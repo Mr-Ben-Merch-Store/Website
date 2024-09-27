703c660e137991af21174f85974f222312121666
stylo-codes-stuff <mistah6909@gmail.com>
2024-09-27 19:31:42 +0000

    added a component that lists all available items stored in the database.(might not work without admin)

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Metadata } from 'next';
-
+import Products from '../components/Products';
 export const metadata: Metadata = {
     title: 'Store',
 };
@@ -9,7 +9,7 @@ export default function store() {
     return (
         <>
             <h1>Store</h1>
-            <hr></hr>
+            <Products/>
         </>
     );
 }
